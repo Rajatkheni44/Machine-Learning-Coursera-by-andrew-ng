a4976410b28edeb2d23a57a3e0f6be78aaebcdf1
Rajat Kheni <rajatkheni44@gmail.com>
2022-06-27 18:34:39 +0200

    Week 7 assignment complited

--- MLDM_ex6/dataset3Params.m
+++ MLDM_ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+%C = 1;
+%sigma = 0.3;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +23,26 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+list = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+error = zeros(length(list),length(list));
+for i = 1:length(list)
+  for j = 1:length(list)
+    C = list(i);
+    sigma = list(j);
+    model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    error(i,j) = mean(double(predictions ~= yval));
+    
+  endfor
+end
 
+[values, row_index]=min(error);
+[~ ,col] = min(values);
+row = row_index(col);
 
-
-
-
+% C and sigma corresponding to min(prediction_error)
+C = list(row);
+sigma = list(col);
 
 % =========================================================================
 
